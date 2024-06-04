b9d812aec65e506b1316801fad3e569448db24c6
ViniciusGavaPereira <vini-gava@outlook.com.br>
2024-06-04 00:30:49 -0300

    Ajuste do arquivo docker para se relacionar com outros containers simultaneamente

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO inventory (Cost,Product, Quantity ) VALUES (10,'Heiniken', 15);
 INSERT INTO inventory (Cost,Product, Quantity ) VALUES (6,'Coca-cola', 4);
 INSERT INTO inventory (Cost,Product, Quantity ) VALUES (8,'Doritos', 3);
 INSERT INTO inventory (Cost,Product, Quantity ) VALUES (5,'Agua', 25);
-INSERT INTO inventory (Cost,Product, Quantity ) VALUES (9,'Hersheis', 15) */
+INSERT INTO inventory (Cost,Product, Quantity ) VALUES (9,'Hersheis', 15)  */
