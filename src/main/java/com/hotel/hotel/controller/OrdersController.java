bba5878385f24a392fcc8dd8623bcb2b6c8bb497
ViniciusGavaPereira <vini-gava@outlook.com.br>
2024-08-13 23:07:34 -0300

    Criacao de endpoint findById

--- src/main/java/com/hotel/hotel/controller/OrdersController.java
+++ src/main/java/com/hotel/hotel/controller/OrdersController.java
@@ -3,12 +3,17 @@ package com.hotel.hotel.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hotel.hotel.dto.InventoryDto;
 import com.hotel.hotel.dto.OrdersDto;
+import com.hotel.hotel.entities.Inventory;
 import com.hotel.hotel.entities.Orders;
 import com.hotel.hotel.service.OrdersService;
 
@@ -25,5 +30,13 @@ public class OrdersController {
 
         List<Orders> result = ordersService.findAll();
         return OrdersDto.ordersConverter(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OrdersDto> findById(@PathVariable Long id) {
+        Orders result = ordersService.findById(id);
+
+        return new ResponseEntity<OrdersDto>(new OrdersDto(result), HttpStatus.OK);
+
     }
 }
--- src/main/java/com/hotel/hotel/service/OrdersService.java
+++ src/main/java/com/hotel/hotel/service/OrdersService.java
@@ -3,10 +3,13 @@ package com.hotel.hotel.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.hotel.hotel.entities.Orders;
 import com.hotel.hotel.repositories.OrdersRepository;
+
+import exception.CustomApplicationException;
 
 @Service
 public class OrdersService {
@@ -16,5 +19,10 @@ public class OrdersService {
 
     public List<Orders> findAll() {
         return ordersRepository.findAll();
+    }
+
+    public Orders findById(Long id) {
+        return ordersRepository.findById(id)
+                .orElseThrow(() -> new CustomApplicationException("Order not found", HttpStatus.NOT_FOUND));
     }
 }
